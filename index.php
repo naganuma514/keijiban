7c1270f3abf30053e41b8af61f8383df0c90eeb1
naganuma514 <naganuma514@icloud.com>
2020-10-13 01:42:40 +0900

    掲示板投稿のみ完了

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
+
+// メッセージを保存するファイルのパス設定
 define( 'FILENAME', './message.txt');
+
+// タイムゾーン設定
 date_default_timezone_set('Asia/Tokyo');
 
+// 変数の初期化
+$now_date = null;
+$data = null;
+$file_handle = null;
+$split_data = null;
+$message = array();
+$message_array = array();
 if( !empty($_POST['btn_submit']) ) {
-    if( $file_handle = fopen( FILENAME, "a") ) {
+	
+	if( $file_handle = fopen( FILENAME, "a") ) {
         $now_date = date("Y-m-d H:i:s");
         $data = "'".$_POST['view_name']."','".$_POST['message']."','".$now_date."'\n";
         fwrite( $file_handle, $data);
@@ -11,8 +23,24 @@ if( !empty($_POST['btn_submit']) ) {
 		// ファイルを閉じる
 		fclose( $file_handle);
 	}	
-	
 }
+if( $file_handle = fopen( FILENAME,'r') ) {
+    while( $data = fgets($file_handle) ){
+
+        $split_data = preg_split( '/\'/', $data);
+        $message = array(
+            'view_name' => $split_data[1],
+            'message' => $split_data[3],
+            'post_date' => $split_data[5]
+        );
+        array_unshift( $message_array, $message);
+    }
+
+    // ファイルを閉じる
+    fclose( $file_handle);
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -37,5 +65,18 @@ if( !empty($_POST['btn_submit']) ) {
 	<input type="submit" name="btn_submit" value="書き込む">
 </form>
 <hr>
+<section>
+<?php if( !empty($message_array) ): ?>
+<?php foreach( $message_array as $value ): ?>
+	<article>
+    <div class="info">
+        <h2><?php echo $value['view_name']; ?></h2>
+        <time><?php echo date('Y年m月d日 H:i', strtotime($value['post_date'])); ?></time>
+    </div>
+    <p><?php echo $value['message']; ?></p>
+</article>
+<?php endforeach; ?>
+<?php endif; ?>
+</section>
 </body>
 </html>
